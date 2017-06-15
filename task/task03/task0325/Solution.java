c9871de3cdf146be1a69769300c2b7f3cd7cc143
unitrade <s.buglak85@gmail.com>
2017-06-15 20:16:43 +0300

    JavaSyntax

---
+++ task/task03/task0325/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task03.task0325;
+
+import java.io.*;
+
+/* 
+Финансовые ожидания
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        System.out.println("Я буду зарабатывать $" + a + " в час");
+    }
+}
