88a466c90fc018c1d4b461d6842cd40c15789b79
unitrade <s.buglak85@gmail.com>
2017-07-10 17:32:59 +0300

    JavaSyntax

---
+++ task/task04/task0418/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task04.task0418;
+
+/* 
+Минимум двух чисел
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+
+        if (a < b) System.out.println(a);
+        if (a > b) System.out.println(b);
+        if (a == b) System.out.println(a);
+    }
+}
